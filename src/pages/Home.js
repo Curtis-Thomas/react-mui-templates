ce38d4c94a54eae013a34b62a85922b7944211ba
fuzzyduckers <83663235+fuzzyduckers@users.noreply.github.com>
2023-10-31 11:29:33 +0200

    init commit

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import { Divider, Typography } from "@mui/material";
+
+function Home() {
+  return (
+    <>
+      <Typography>MUI Templates</Typography>
+      <Typography>A collection of React templates which use MUI.</Typography>
+
+      <Divider />
+      <Typography>Categories</Typography>
+      <Typography>Dashboard</Typography>
+      <Typography>Landing Page</Typography>
+    </>
+  );
+}
+
+export default Home;
